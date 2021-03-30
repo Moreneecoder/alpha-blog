2e6d5ec2403a26a35a84e694557252e16446ff0f
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-30 23:27:21 +0100

    Add create and edit features also flash messaging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,26 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(user_params)
+
+    # if @article.valid?
+    if @article.save
+      flash[:notice] = "Article saved successfully"
+      redirect_to @article # article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:article).permit(:title, :description)
+  end
 end
